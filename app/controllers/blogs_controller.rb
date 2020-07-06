ca7287f3ffe6133f2d4d5c0f04aa28996d97cd08
vagrant <vagrant@localhost.localdomain>
2020-07-06 02:34:05 +0000

    [finish]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,10 +14,17 @@ class BlogsController < ApplicationController
   def create
   	blog = Blog.new(blog_params)
   	blog.save
-  	redirect_to blogs_path
+  	redirect_to blog_path(blog.id)
   end
 
   def edit
+  	@blog = Blog.find(params[:id])
+  end
+
+  def update
+  	blog = Blog.find(params[:id])
+  	blog.update(blog_params)
+  	redirect_to blog_path(blog)
   end
 
   private
